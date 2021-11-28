af2ce45f6f558e0c588f6c5a5448e80d86f3b469
Moaad Fattali <moaad.fattali@ovrsea.com>
2021-11-28 20:01:41 +0100

    implem distibution of 20E bill

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,4 +4,8 @@ describe("ATM", function () {
   it("should return 10€ bill when requested 10€", function () {
     expect(atm(10)).toEqual({ 10: 1 });
   });
+
+  it("should return 20€ bill when requested 20€", function () {
+    expect(atm(20)).toEqual({ 20: 1 });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ type availableBill = 10 | 20 | 50 | 100 | 200 | 500;
 type distributedBill = Partial<Record<availableBill, number>>;
 
 export const atm = (requestedAmount: number): distributedBill => {
-  return { 10: 1 };
+  return { [requestedAmount]: 1 };
 };
