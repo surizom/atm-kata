807a0fd4c6ce57a8c00fe8dbec7cc1a7014b6fcf
Moaad Fattali <moaad.fattali@ovrsea.com>
2021-11-28 20:28:43 +0100

    impkemn distribution of bills by desc order

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,15 @@
-import { atm } from ".";
+import { atm, initialDistribution } from ".";
 
 describe("ATM", function () {
   it("should return 10€ bill when requested 10€", function () {
-    expect(atm(10)).toEqual({ 10: 1 });
+    expect(atm(10)).toEqual({ 10: 1, ...initialDistribution });
   });
 
   it("should return 20€ bill when requested 20€", function () {
-    expect(atm(20)).toEqual({ 20: 1 });
+    expect(atm(20)).toEqual({ 20: 1, ...initialDistribution });
+  });
+
+  it("should return 2 * 20€ bill when requested 30€", function () {
+    expect(atm(40)).toEqual({ 20: 2, ...initialDistribution });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,42 @@
-type availableBill = 10 | 20 | 50 | 100 | 200 | 500;
+const AVAILABLE_BILLS_OBJECT = { 10: "10", 20: "20", 50: "50", 100: "100", 200: "200", 500: "500" };
 
-type distributedBill = Partial<Record<availableBill, number>>;
+const AVAILABLE_BILLS: number[] = Object.keys(AVAILABLE_BILLS_OBJECT).map((bill) => parseInt(bill));
 
-export const atm = (requestedAmount: number): distributedBill => {
-  return { [requestedAmount]: 1 };
+type AvailableBill = keyof typeof AVAILABLE_BILLS_OBJECT;
+
+type DistributedBills = Partial<Record<AvailableBill, number>>;
+
+type DistributionState = {
+  distributedBills: DistributedBills;
+  remainingAmount: number;
+};
+
+const reduceAmount = (previous: DistributionState, currentBillType: AvailableBill): DistributionState => {
+  if (previous.remainingAmount >= currentBillType) {
+    return {
+      distributedBills: {
+        ...previous.distributedBills,
+        [currentBillType]: previous.remainingAmount % currentBillType,
+      },
+      remainingAmount: previous.remainingAmount - (previous.remainingAmount % currentBillType),
+    };
+  } else {
+    return {
+      distributedBills: previous.distributedBills,
+      remainingAmount: previous.remainingAmount,
+    };
+  }
+};
+
+export const initialDistribution: DistributedBills = { 10: 0, 20: 0, 50: 0, 100: 0, 200: 0, 500: 0 };
+
+export const atm = (requestedAmount: number): DistributedBills => {
+  const initialState: DistributionState = {
+    distributedBills: initialDistribution,
+    remainingAmount: requestedAmount,
+  };
+
+  const truc = AVAILABLE_BILLS.reduce<DistributionState>(reduceAmount, initialState);
+
+  return truc.distributedBills;
 };
