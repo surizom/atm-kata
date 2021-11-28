396d7b4012ba0626f545e5b64ed1c29e1b578582
Moaad Fattali <moaad.fattali@ovrsea.com>
2021-11-28 20:00:47 +0100

    feat: basic distribution of 10 euro bill"
    implem distibution of 20E bill
    
    impkemn distribution of bills by desc order
    
    add test
    
    refactor
    
    truc

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,23 @@
-import { systemUnderTest } from ".";
+import { atm, initialDistribution } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("ATM", function () {
+  it("should return 10€ bill when requested 10€", function () {
+    expect(atm(10)).toEqual({ ...initialDistribution, 10: 1 });
+  });
+
+  it("should return 20€ bill when requested 20€", function () {
+    expect(atm(20)).toEqual({ ...initialDistribution, 20: 1 });
+  });
+
+  it("should return 2 * 20€ bill when requested 30€", function () {
+    expect(atm(40)).toEqual({ ...initialDistribution, 20: 2 });
+  });
+
+  it("should return 1 * 50€ + 1*10 bill when requested 60€", function () {
+    expect(atm(60)).toEqual({ ...initialDistribution, 50: 1, 10: 1 });
+  });
+
+  it("should return 1 * 500€ + 1 * 200€ and 1 * 20 when requested 720€", function () {
+    expect(atm(720)).toEqual({ ...initialDistribution, 500: 1, 200: 1, 20: 1 });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
-export function systemUnderTest(): boolean {
-  return true;
-}
+const AVAILABLE_BILLS_OBJECT = { 10: "10", 20: "20", 50: "50", 100: "100", 200: "200", 500: "500" };
+
+const AVAILABLE_BILLS: AvailableBill[] = [500, 200, 100, 50, 20, 10];
+
+type AvailableBill = keyof typeof AVAILABLE_BILLS_OBJECT;
+
+type DistributedBills = Record<AvailableBill, number>;
+
+type DistributionState = {
+  distributedBills: DistributedBills;
+  remainingAmount: number;
+};
+
+const canDistribute = (amount: number, billType: AvailableBill) => amount >= billType;
+
+const euclidianDivision = (n: number, quotient: number) => {
+  const reste = n % quotient;
+
+  return (n - reste) / quotient;
+};
+
+const distributeAmount = (
+  currentDistribution: DistributionState,
+  currentBillType: AvailableBill
+): DistributionState => {
+  const distributedBillsForCurrentBill = euclidianDivision(currentDistribution.remainingAmount, currentBillType);
+
+  return {
+    distributedBills: {
+      ...currentDistribution.distributedBills,
+      [currentBillType]: distributedBillsForCurrentBill,
+    },
+    remainingAmount: currentDistribution.remainingAmount - currentBillType * distributedBillsForCurrentBill,
+  };
+};
+
+export const initialDistribution: DistributedBills = { 10: 0, 20: 0, 50: 0, 100: 0, 200: 0, 500: 0 };
+
+export const atm = (requestedAmount: number): DistributedBills => {
+  const initialState: DistributionState = {
+    distributedBills: initialDistribution,
+    remainingAmount: requestedAmount,
+  };
+
+  const distribution = AVAILABLE_BILLS.reduce<DistributionState>(distributeAmount, initialState);
+
+  return distribution.distributedBills;
+};
